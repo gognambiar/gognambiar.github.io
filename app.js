b7875aaa6d36eaf56df725f9b43224ab64905dad
Gautam OG <gognambiar@gmail.com>
2017-10-23 19:33:04 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,90 @@
+var lstofwords = [
+["T", "R", "E", "E", "H", "O", "U", "S", "E"],
+  ["J","A","V","A","S","C","R","I","P","T"],
+  ["W","E","B","D","E","S","I","G","N"],
+  ["E","D","U","C","A","T","I","O","N"],
+  ["C","H","O","C","O","L","A","T","E"],
+  ["G","E","R","M","A","N","Y"]
+]
+
+var random = Math.floor((Math.random()*(lstofwords.length-1))); 
+
+var nword = lstofwords[random]; // the word to guess will be chosen from the array above
+var blkspaces = new Array(nword.length);
+var numattempts = 0;
+
+// every letter in the word is symbolized by an underscore in the guessfield
+for (var i = 0; i < blkspaces.length; i++){
+	blkspaces[i] = "_ ";
+}
+
+function wordload(){
+   $.get('smpl.txt', function(data) {
+    var lines = data.split("\n");
+    lines.map(function(x){ return x.toUpperCase() })
+    console.log(lines[0]);
+});
+}
+
+// prints the guessfield
+function printblkspaces(){
+	for (var i = 0; i < blkspaces.length; i++){
+	var dblkspcs = document.getElementById("dblkspcs");
+	var spcs = document.createTextNode(blkspaces[i]);
+	dblkspcs.appendChild(spcs);
+	}
+}
+
+//checks if the the letter provided by the user matches one or more of the letters in the word
+var checkLetter = function(){
+	var f = document.hgfrm; 
+	var b = f.elements["inplet"]; 
+	var ltr = b.value; // the letter provided by the user
+	ltr = ltr.toUpperCase();
+	for (var i = 0; i < nword.length; i++){
+		if(nword[i] === ltr){
+			blkspaces[i] = ltr + " ";
+			var corr = true;
+		}
+
+	b.value = "";
+	}
+	
+	//deletes the guessfield and replaces it with the new one
+	var dblkspcs = document.getElementById("dblkspcs");
+	dblkspcs.innerHTML=""; 
+	printblkspaces();
+	
+	// if a guessed letter is not in the word, the letter will be put on the "wrong letters"-list and hangman grows
+	if(!corr){
+		var incorrectletters = document.getElementById("incorrectletters");
+		var spcs = document.createTextNode(" " + ltr);
+		incorrectletters.appendChild(spcs); 
+		numattempts++;
+		var hangman = document.getElementById("hangman");
+    hangman.src = "http://www.writteninpencil.de/Projekte/Hangman/hangman" + numattempts + ".png";
+	}
+	
+	//checks if all letters have been found
+	var winflag = true;
+	for (var i = 0; i < blkspaces.length; i++){
+		if(blkspaces[i] === "_ "){
+			winflag = false;
+		}
+	}
+	if(winflag){
+		window.alert("You win!");
+	}
+	
+	//once you got six wrong letters, you lose
+	if(numattempts === 6){
+		window.alert("You Lose, please reset the game!");
+	}
+}
+
+function init(){
+	//wordload();
+	printblkspaces();
+}
+
+window.onload = init;
