0e8de4511b0e46b98ab0cc621def8fd5bc83d266
Gautam OG <gognambiar@gmail.com>
2017-10-23 20:27:16 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,15 @@
   ["C","H","O","C","O","L","A","T","E"],
   ["G","E","R","M","A","N","Y"]
 ]*/
-var lstofwords = [[]];
 
-$.get('smpl.txt', function(data) {
+/*$.get('smpl.txt', function(data) {
     var lstofwords = data.split("\n");
     lstofwords.map(function(x){ return x.toUpperCase() });
 });
+*/
+$.get('smpl.txt', function(data) {
+   var lstofwords = data.split("\n");
+}, 'text');
 
 var random = Math.floor((Math.random()*(lstofwords.length-1))); 
 
