a7250bb79731a78310c4173540e6bdeab13d2e89
Gautam OG <gognambiar@gmail.com>
2017-10-24 09:57:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ $.get('smpl.txt', function(data) {
 })
 
 function selword(){
-var random = Math.floor((Math.random()*(lstofwords.length-1))); 
-var nword = lstofwords[random]; // the word to guess will be chosen from the array above
+random = Math.floor((Math.random()*(lstofwords.length-1))); 
+nword = lstofwords[random]; // the word to guess will be chosen from the array above
 blkspaces = new Array(nword.length);
 
 // every letter in the word is symbolized by an underscore in the guessfield
