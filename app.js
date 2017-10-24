4890df4107154bbc9d77ab0f298b63afdc04c4be
Gautam OG <gognambiar@gmail.com>
2017-10-23 22:06:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var numattempts = 0;
 for (var i = 0; i < blkspaces.length; i++){
 	blkspaces[i] = "_ ";
 }
-});
+
 // prints the guessfield
 function printblkspaces(){
 	for (var i = 0; i < blkspaces.length; i++){
@@ -83,3 +83,4 @@ function init(){
 	printblkspaces();
 }
 window.onload = init;
+});
