4ec352896141262740ba1939461e9672d1f5d1d6
Gautam OG <gognambiar@gmail.com>
2018-06-21 11:56:22 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $.ajax({
     type: 'POST',
-    url: 'http://localhost:3000/twt',
+    url: 'https://ogckw8uu9b.execute-api.us-east-1.amazonaws.com/DummyStage/greeting/hello',
     success: function(data) {
     	console.log(data);
 	alert("Hi There!");
