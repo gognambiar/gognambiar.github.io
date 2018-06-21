31e2e41bfaa01a6e39c955974637bb8ba82a5c76
Gautam OG <gognambiar@gmail.com>
2018-06-21 12:01:28 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ $.ajax({
     type: 'POST',
     url: 'https://ogckw8uu9b.execute-api.us-east-1.amazonaws.com/DummyStage/greeting/hello',
     success: function(data) {
-    	console.log(data);
-	alert("Hi There!");
+    	console.log("Success!!");
       }
 });
